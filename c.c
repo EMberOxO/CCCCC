704fcbd64755b6d3aa7edabdfe005bbf166df3ca
TaoGuohss <49747779+TaoGuohss@users.noreply.github.com>
2019-07-23 15:11:28 +0800

    Add files via upload

---
+++ c.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#define true 1
+#define false 0
+int nu(int num) //是否素数
+{
+	int i;
+	for(i=2;i<num;i++)
+	{
+		if(num%i==0)
+		{
+		break;
+		}
+	}
+	if(num==i)
+	return 1;
+	else
+	return 0;
+}
+
+int main(void)
+{
+	int a;
+	int b;
+	int i;
+	printf("请输入一个偶数:");
+	scanf("%d",&a);
+	if(a%2!=0)
+	{
+		printf("输入的数不是偶数");
+		return 0;
+	}
+	for(i=1;i<a;i+=2)
+	{
+		if(nu(i),nu(b))
+		{
+		b=a-i;
+		printf("%d+%d=%d\n",b,a-b,a);
+		}
+	}
+	return 0;
+}
